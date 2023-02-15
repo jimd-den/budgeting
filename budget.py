e7f8337530ceed683ace317a0429cbdaff62d846
James Davis <james_d_davis@outlook.com>
2023-02-15 02:11:41 +0000

    organizatin

--- budget.py
+++ budget.py
@@ -8,6 +8,7 @@ add_transaction:
 
 """
 
+import transaction_manager
 import random
 import bud_technical
 import sys
@@ -17,41 +18,6 @@ import time
 ## our main object is the transaction. Every Bill is a transaction, income is a transaction.
 
 
-class Transaction:
-    def __init__(self, transaction_id, amount, date, reason, transaction_type):
-        self.transaction_id = transaction_id
-        self.amount = amount
-        self.date = date
-        self.reason =  reason
-        self.transaction_type = transaction_type
-
-
-def remove_transaction():
-    print("takes the id of the transaction")
-
-def add_transaction():
-    date = bud_technical.numeric_input("Date: ", 
-                         "Please refer to calendar for numeric date:")
-    amount = bud_technical.numeric_input("Amount: ",
-                           "Please enter a dollar amount")
-    transaction_type = bud_technical.numeric_input("income(1) or expense(0): ",
-                                     "Please enter a 1 or a 0")
-    reason = input("reason: ")
-    
-    buffer_transaction = Transaction(random.randint(0,3000),
-                                     amount,
-                                     date,
-                                     reason,
-                                     transaction_type)
-    if transaction_type == 0:
-        buffer_transaction.amount = 0 - buffer_transaction.amount
-    transaction_tuple = (buffer_transaction.amount,
-                         buffer_transaction.date,
-                         buffer_transaction.reason)
-    for i in transaction_tuple:
-        print(i)
-    insert_trans = bud_technical.insert_transaction(transaction_tuple)
-    print("logged record number: ", insert_trans)
     
 
 if __name__ == "__main__":
@@ -65,10 +31,10 @@ if __name__ == "__main__":
     if len(args) > 1:
         match args[1]:
             case "--add_transaction":
-                add_transaction()
+               transaction_manager.add_transaction()
             case "--remove_transaction":
-               remove_transaction()
+               transaction_manager.remove_transaction()
             case "--show_snapshot":
-                show_snapshot()
+               transaction_manager.show_snapshot()
             case "--create_db":
                 bud_technical.create_database()
